efe986632c0515f9f2c23d5e65b21ca0c98cc869
Adam Kennedy <adam.kennedy@oregonstate.edu>
2016-02-18 22:59:06 -0800

    changed streamnet_dh func call name

--- streamnet_dh.m
+++ streamnet_dh.m
@@ -1,4 +1,4 @@
-function [msg,status] = streamt_data_harvester_sql(source,qc_source,template,sitecode,profile,pn_dest,pn_plots,html,email,fn_dest,reprocess)
+function [msg,status] = streamnet_dh(source,qc_source,template,sitecode,profile,pn_dest,pn_plots,html,email,fn_dest,reprocess)
 %Data harvester function template for post-processing streaming sensor data retrieved from an SQL data source
 %
 %syntax: [msg,status] = data_harvester_sql(source,template,sitecode,profile,pn_dest,pn_plots,html,email,fn_dest)
